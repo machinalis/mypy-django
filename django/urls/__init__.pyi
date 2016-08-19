bbc169df1d4164c2c41c17fc2da3ec79d8b271c5
Daniel F Moisset <dmoisset@machinalis.com>
2016-08-19 20:31:55 +0100

    Remove warnings due to incomplete stubs

--- django/urls/__init__.pyi
+++ django/urls/__init__.pyi
@@ -1,9 +1,9 @@
-from .base import (
+from .base import (  # type: ignore  # stub not done yet
     clear_script_prefix, clear_url_caches, get_script_prefix, get_urlconf,
     is_valid_path, resolve, reverse, reverse_lazy, set_script_prefix,
     set_urlconf, translate_url,
 )
-from .exceptions import NoReverseMatch, Resolver404
+from .exceptions import NoReverseMatch, Resolver404  # type: ignore  # stub not done yet
 from .resolvers import (
     LocaleRegexProvider, LocaleRegexURLResolver, RegexURLPattern,
     RegexURLResolver, ResolverMatch, get_ns_resolver, get_resolver,
